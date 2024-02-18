8bbaa483f155af7dad15fe64a0c52675e01a687a
Tibor <112723857+Tibor0106@users.noreply.github.com>
2024-02-18 08:07:17 +0100

    create router

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
+
 require_once "DatabaseActions/Database.php";
 $databaseAction = new DatabaseAction();
-echo $databaseAction->Select("SELECT * FROM blogs")->response;
+
+$request = $_SERVER["REQUEST_URI"];
+
+$folder = "/PHPFRAMEWORK/";
+switch ($request) {
+    case $folder."getBlogMessages":
+        echo $databaseAction->Select("SELECT * FROM blogs")->response;
+        break;
+    case $folder."home":
+        require "./Pages/index.html";
+    default:
+        http_response_code(404);   
+}
  ?>
